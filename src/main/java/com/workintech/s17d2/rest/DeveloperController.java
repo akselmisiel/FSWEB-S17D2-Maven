f5f7a1c8b0ef586ad8b038b74a62fef4870ad2f7
Aksel <akselmisiel@gmail.com>
2024-06-30 02:47:14 +0300

    Done

--- src/main/java/com/workintech/s17d2/rest/DeveloperController.java
+++ src/main/java/com/workintech/s17d2/rest/DeveloperController.java
@@ -4,6 +4,7 @@ package com.workintech.s17d2.rest;
 import com.workintech.s17d2.model.Developer;
 import com.workintech.s17d2.model.Experience;
 import com.workintech.s17d2.model.JuniorDeveloper;
+import com.workintech.s17d2.model.MidDeveloper;
 import com.workintech.s17d2.tax.Taxable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpStatus;
@@ -27,6 +28,8 @@ public class DeveloperController {
     public void init() {
         developers = new HashMap<>();
         developers.put(1, new JuniorDeveloper(1, "John", 1000d));
+        developers.put(2, new MidDeveloper(2, "Jane", 2000d));
+        developers.put(3, new JuniorDeveloper(3, "Doe", 3000d));
     }
 
     @GetMapping
@@ -34,30 +37,30 @@ public class DeveloperController {
         return developers.values().stream().toList();
     }
 
-    @GetMapping("{id}")
-    public Developer getDeveloper(Integer id) {
+    @GetMapping("/{id}")
+    public Developer getDeveloper(@PathVariable Integer id) {
         return developers.get(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void addDeveloper(Integer id, String name, Double salary, Experience experience) {
-        if (experience != null) {
-            switch (experience) {
-                case JUNIOR -> developers.put(id, new JuniorDeveloper(id, name, salary - (salary * taxable.getSimpleTaxRate())));
-                case MID -> developers.put(id, new JuniorDeveloper(id, name, salary - (salary * taxable.getMiddleTaxRate())));
-                case SENIOR -> developers.put(id, new JuniorDeveloper(id, name, salary - (salary * taxable.getUpperTaxRate())));
+    public void addDeveloper(@RequestBody Developer developer) {
+        if (developer.getExperience() != null) {
+            switch (developer.getExperience()) {
+                case JUNIOR -> developers.put(developer.getId(), new JuniorDeveloper(developer.getId(), developer.getName(), developer.getSalary() - (developer.getSalary() * taxable.getSimpleTaxRate())));
+                case MID -> developers.put(developer.getId(), new MidDeveloper(developer.getId(), developer.getName(), developer.getSalary() - (developer.getSalary() * taxable.getMiddleTaxRate())));
+                case SENIOR -> developers.put(developer.getId(), new MidDeveloper(developer.getId(), developer.getName(), developer.getSalary() - (developer.getSalary() * taxable.getUpperTaxRate())));
             }
         }
     }
 
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     public void updateDeveloper(@PathVariable Integer id, @RequestBody Developer developer) {
         developers.replace(id, developer);
     }
 
-    @DeleteMapping("{id}")
-        public void deleteDeveloper(Integer id) {
+    @DeleteMapping("/{id}")
+        public void deleteDeveloper(@PathVariable Integer id) {
         developers.remove(id);
     }
 
